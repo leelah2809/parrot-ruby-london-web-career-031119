35bee97de6022516c03c9a9c8a233e0708a48191
Okunola Jelilat Dasola <jelilatokunola2809@gmail.com>
2019-02-14 23:53:44 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,6 @@
-# Create method `parrot` that outputs a given phrase and
+def parrot(name = 'Squawk!')
+ puts name
+ return name# Create method `parrot` that outputs a given phrase and
 # returns the phrase
+end
+parrot ('Owl!')
